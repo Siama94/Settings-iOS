23a91bcd0029a7361f2fcd3f5f4419990e5f2031
Anastasiia Maksimova <sia.m94@ya.ru>
2021-07-24 01:37:22 +0500

    Add all cells.

--- Settings iOS/ViewController.swift
+++ Settings iOS/ViewController.swift
@@ -110,10 +110,119 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
                         label: "Не подключено",
                         handler: {print("Нажата ячейка Wi-Fi")})),
             
-            .standardCell(model:  SettingStandardOption(title: "Сотовая связь",
+            .standardCell(model: SettingStandardOption(
+                            title: "Сотовая связь",
                             icon: UIImage(systemName: "antenna.radiowaves.left.and.right"),
                             iconBackgroundColor: .systemGreen,
                             handler: {print("Нажата ячейка Сотовая связь")})),
+            
+            .labelCell(model: SettingLabelOptions(
+                        title: "Bluetooth",
+                        icon: UIImage(named: "блютуз"),
+                        iconBackgroundColor: .systemBlue,
+                        label: "Вкл.",
+                        handler: {print("Нажата ячейка Bluetooth")})),
+            
+            .standardCell(model: SettingStandardOption(
+                            title: "Сотовая связь",
+                            icon: UIImage(systemName: "antenna.radiowaves.left.and.right"),
+                            iconBackgroundColor: .systemGreen,
+                            handler: {print("Нажата ячейка Сотовая связь")})),
+            
+            .standardCell(model: SettingStandardOption(
+                            title: "Режим модема",
+                            icon: UIImage(systemName: "personalhotspot"),
+                            iconBackgroundColor: .systemGreen,
+                            handler: {print("Нажата ячейка Режим модема")})),
+            
+            .switchCell(model: SettingSwitchOptions(
+                            title: "VPN",
+                            icon: UIImage(systemName: "globe"),
+                            iconBackgroundColor: .systemBlue,
+                            handler: {print("Нажата ячейка VPN")}, isOn: false))
+            ]))
+      
+        
+        models.append(Sections(options: [
+            .standardCell(model: SettingStandardOption(
+                            title: "Уведомления",
+                            icon: UIImage(systemName: "note"),
+                            iconBackgroundColor: .systemRed,
+                            handler: {print("Нажата ячейка Уведомления")})),
+            
+            .standardCell(model: SettingStandardOption(
+                            title: "Звуки, тактильные сигналы",
+                            icon: UIImage(systemName: "speaker.wave.3.fill"),
+                            iconBackgroundColor: .systemPink,
+                            handler: {print("Нажата ячейка Звуки, тактильные сигналы")})),
+            
+            .standardCell(model:  SettingStandardOption(
+                            title: "Не беспокоить",
+                            icon: UIImage(systemName: "moon.fill"),
+                            iconBackgroundColor: .systemIndigo,
+                            handler: {print("Нажата ячейка Не беспокоить")})),
+            
+            .standardCell(model: SettingStandardOption(
+                            title: "Экранное время",
+                            icon: UIImage(systemName: "hourglass"),
+                            iconBackgroundColor: .systemIndigo,
+                            handler: {print("Нажата ячейка Экранное время")})),
+        ]))
+        
+        models.append(Sections(options: [
+            .standardCell(model: SettingStandardOption(
+                            title: "Основные",
+                            icon: UIImage(systemName: "gear"),
+                            iconBackgroundColor: .systemGray,
+                            handler: {print("Нажата ячейка Основные")})),
+            
+            .standardCell(model: SettingStandardOption(
+                            title: "Пункт управления",
+                            icon: UIImage(systemName: "slider.horizontal.3"),
+                            iconBackgroundColor: .systemGray,
+                            handler: {print("Нажата ячейка Пункт управления")})),
+            
+            .standardCell(model:  SettingStandardOption(
+                            title: "Экран и яркость",
+                            icon: UIImage(systemName: "textformat.size"),
+                            iconBackgroundColor: .systemBlue,
+                            handler: {print("Нажата ячейка Экран и яркость")})),
+            
+            .standardCell(model: SettingStandardOption(
+                            title: "Экран «‎Домой»‎",
+                            icon: UIImage(systemName: "apps.iphone"),
+                            iconBackgroundColor: .systemIndigo,
+                            handler: {print("Нажата ячейка Экран «‎Домой")})),
+            
+            .standardCell(model:   SettingStandardOption(
+                            title: "Универсальный доступ",
+                            icon: UIImage(systemName: "figure.stand"),
+                            iconBackgroundColor: .systemBlue,
+                            handler: {print("Нажата ячейка Универсальный доступ")})),
+            
+            .standardCell(model: SettingStandardOption(
+                            title: "Обои",
+                            icon: UIImage(systemName: "seal"),
+                            iconBackgroundColor: .systemTeal,
+                            handler: {print("Нажата ячейка Обои")})),
+            
+            .standardCell(model:  SettingStandardOption(
+                            title: "Face ID и код-пароль",
+                            icon: UIImage(systemName: "person.fill.viewfinder"),
+                            iconBackgroundColor: .systemGreen,
+                            handler: {print("Нажата ячейка Face ID и код-пароль")})),
+            
+            .standardCell(model:  SettingStandardOption(
+                            title: "Аккамулятор",
+                            icon: UIImage(systemName: "battery.100"),
+                            iconBackgroundColor: .systemGreen,
+                            handler: {print("Нажата ячейка Аккамулятор")})),
+            
+            .standardCell(model: SettingStandardOption(
+                            title: "Конфиденциальность",
+                            icon: UIImage(systemName: "hand.raised.fill"),
+                            iconBackgroundColor: .systemBlue,
+                            handler: {print("Нажата ячейка Конфиденциальность")})),
         ]))
     }
     
@@ -160,5 +269,4 @@ struct SettingLabelOptions {
     let iconBackgroundColor: UIColor
     var label: String
     let handler: (() -> Void)
-    
 }
